a4407f2c4effa4cdaffdac07df8588ada4d1f7ec
wea9677 <wea9677@naver.com>
2022-06-29 20:19:24 +0900

    변경사항 병합

--- models/users.js
+++ models/users.js
@@ -1,18 +1,29 @@
-const mongoose = require("mongoose");
-
-
-const UserSchema = mongoose.Schema(
-    {
-        userId : {
-            type : String,
-            require : true,
-            unique:true
-        },
-        nickname:String,
-        userImage:String,
-        email:String
-        
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
     }
-);
-
-module.exports = mongoose.model("User", UserSchema);
+  }
+  User.init({
+    userId : {
+      type:DataTypes.STRING,
+      unique:true
+    },
+    nickname: DataTypes.STRING,
+    userImage:DataTypes.STRING,
+    email : DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
